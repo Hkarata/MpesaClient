7063821c6639595a73e66f8aed1dfa3427160f36
hei karata <herishine00@gmail.com>
2024-05-09 01:42:58 +0300

    `Added ApiClient.cs and updated DocumentLayout.json`
    Added a new file `ApiClient.cs` to the `MpesaClient` namespace, defining the `ApiClient` class with several private fields for API interaction. Updated `DocumentLayout.json` to reflect changes in the project structure and opened documents, including the replacement of `ApiMethodTypes.cs` references with `ApiClient.cs` and the addition of `ApiContext.cs`.

---
+++ MpesaClient/ApiClient.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel;
+
+namespace MpesaClient
+{
+    public class ApiClient
+    {
+        [Description("This is the API key obtained from the Mpesa portal, used for service access.")]
+        private string apiKey = "";
+
+        [Description("This is the public key obtained from the Mpesa portal, used for encrypting the request payload.")]
+        private string publicKey = "";
+
+        [Description("This is a boolean value that determines whether the request should be made over a secure connection.")]
+        private bool ssl = false;
+
+        [Description("This is the type of HTTP method to be used in making the request.")]
+        private ApiMethodTypes methodType = ApiMethodTypes.GET;
+
+        [Description("This is the address of the API endpoint.")]
+        private string address = "";
+
+        [Description("This is the port number to be used in making the request. The default port is 80")]
+        private int port = 80;
+
+        [Description("This is the path to the API endpoint.")]
+        private string path = "";
+
+        [Description("This is a dictionary of headers to be included in the request.")]
+        private Dictionary<string, string> headers = new Dictionary<string, string>();
+
+        [Description("This is a dictionary of parameters to be included in the request.")]
+        private Dictionary<string, string> parameters = new Dictionary<string, string>();
+    }
+}
