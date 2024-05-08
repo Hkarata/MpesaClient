d77a2f4abf216d7d04ad77d05768116208073594
hei karata <herishine00@gmail.com>
2024-05-09 02:57:59 +0300

    Updated DocumentLayout.json and added Description attribute in Country.cs
    Updated the references, child index, document index, and view state in DocumentLayout.json. Added the System.ComponentModel namespace and Description attribute to constants in Country.cs for descriptive text.

--- MpesaClient/Markets/Country.cs
+++ MpesaClient/Markets/Country.cs
@@ -1,10 +1,19 @@
-﻿namespace MpesaClient.Markets
+﻿using System.ComponentModel;
+
+namespace MpesaClient.Markets
 {
     public static class Country
     {
+        [Description("Short Code for Ghana")]
         public const string GHA = "GHA";
+
+        [Description("Short Code for Tanzania")]
         public const string TZN = "TZN";
+
+        [Description("Short Code for Lesotho")]
         public const string LES = "LES";
+
+        [Description("Short Code for Democratic Republic of Congo")]
         public const string DRC = "DRC";
     }
 }
