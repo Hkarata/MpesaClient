6bc423151987865f815d57346f12a6ff7b09ccd7
hei karata <herishine00@gmail.com>
2024-05-09 01:32:29 +0300

    Updated DocumentLayout.json and added ApiMethodTypes.cs
    Updated `DocumentLayout.json` with a new version and workspace root path, including a new document, `ApiMethodTypes.cs`, in the `MpesaClient` project. The layout of the document groups has been updated with new bookmarks and the `ApiMethodTypes.cs` document added to the first document group. Additionally, a new file, `ApiMethodTypes.cs`, has been added to the `MpesaClient` project. This file defines a new enum, `ApiMethodTypes`, in the `MpesaClient` namespace with four values: `GET`, `POST`, `PUT`, and `DELETE`, each with a corresponding description.

---
+++ MpesaClient/ApiMethodTypes.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+
+namespace MpesaClient
+{
+    public enum ApiMethodTypes
+    {
+        [Description("Get request")]
+        GET = 0,
+
+        [Description("Post request")]
+        POST = 1,
+
+        [Description("Put request")]
+        PUT = 3,
+
+        [Description("Delete request")]
+        DELETE = 4,
+    }
+}
